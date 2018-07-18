efb4a0ededd8a3fd7d2a22b88f5244c15ff2591a
Andrey-Scartazzini <andreyfeldmann@gmail.com>
2018-07-18 14:47:15 -0300

    FIM
    Com o banco atualizado

--- bd.sql
+++ bd.sql
@@ -24,9 +24,9 @@ DROP TABLE IF EXISTS `categorias`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `categorias` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) DEFAULT NULL,
+  `nome` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,7 +35,7 @@ CREATE TABLE `categorias` (
 
 LOCK TABLES `categorias` WRITE;
 /*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
-INSERT INTO `categorias` VALUES (1,'esportes'),(2,'escolar'),(3,'mobilidade');
+INSERT INTO `categorias` VALUES (1,'esporte'),(2,'escolar'),(15,'mobilidade');
 /*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -48,14 +48,15 @@ DROP TABLE IF EXISTS `produtos`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `produtos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) DEFAULT NULL,
+  `nome` varchar(255) NOT NULL,
   `preco` decimal(10,2) DEFAULT NULL,
   `descricao` longtext,
   `categoria_id` int(11) DEFAULT NULL,
+  `usado` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `fk_produto_categoria_idx` (`categoria_id`),
   CONSTRAINT `fk_produto_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,13 +65,34 @@ CREATE TABLE `produtos` (
 
 LOCK TABLES `produtos` WRITE;
 /*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
-INSERT INTO `produtos` VALUES (1,'arroz',35.60,NULL,1),(2,'feijão',12.80,NULL,2);
+INSERT INTO `produtos` VALUES (42,'Caderno',18.00,'20 matérias',2,0),(43,'Caneta azul',1.00,'é azul',2,0),(44,'Bike',200.00,'ainda é boa',15,1),(45,'Bola de vôlei',78.00,'Vôlei é legal',1,0);
 /*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Dumping events for database 'curso_1'
+-- Table structure for table `usuario`
 --
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'andreyscartazzini@gmail.com','202cb962ac59075b964b07152d234b70'),(2,'outro@email.com','202cb962ac59075b964b07152d234b70');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -81,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-16 11:15:25
+-- Dump completed on 2018-07-18 14:21:40
